f58ae7de0c33ef3423b56aeecc40d2c8cf58605a
Maxoverking <avemax7@gmail.com>
2022-10-14 14:38:53 +0200

    сделал 3 задание

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,8 @@
 import Player from '@vimeo/player';
 import { throttle } from "lodash";
 
-  const iframe = document.querySelector('iframe');
+const iframe = document.querySelector('iframe');
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
 
 // console.log("🚀 ~ iframe", iframe);
 
@@ -11,10 +12,6 @@ player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay(event) {
     // console.log("🚀 ~ e", event.seconds);   
-localStorage.setItem("videoplayer-current-time", event.seconds);
+localStorage.setItem(LOCALSTORAGE_KEY, event.seconds);
 };
-
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
-    .catch(function (error) {
-        console.log(error);
-});   
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));   
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import { throttle } from "lodash";
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(formFill, 500));
+form.addEventListener('submit', submitForm);
+
+function formFill(){
+    const newObjSave = { email: email.value , message: textarea.value };
+    // console.log("🚀 ~ formObj", newObjSave);
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(newObjSave));
+};
+
+function submitForm(evt) {
+    evt.preventDefault();
+    console.log({ email: email.value, message: textarea.value });
+    form.reset();
+    localStorage.getItem(LOCALSTORAGE_KEY);
+};
+
+function loadingStorage () {
+    const getItemLOCALSTORAGE_KEY = localStorage.getItem(LOCALSTORAGE_KEY);
+    // return getItemLOCALSTORAGE_KEY;
+    return getItemLOCALSTORAGE_KEY === null ? undefined : JSON.parse(getItemLOCALSTORAGE_KEY);
+};
+
+const storageData = loadingStorage();
+
+// console.log("🚀 ~ storageData", storageData);
+if (storageData) {
+  email.value = storageData.email;
+  textarea.value = storageData.message;
+};
+
+
+
+
+    
