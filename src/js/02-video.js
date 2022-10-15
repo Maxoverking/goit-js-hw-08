c3552410293b8a7732655a7feb0eba65476bf046
Maxoverking <avemax7@gmail.com>
2022-10-15 14:49:10 +0200

    исправил уловие

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,15 +3,19 @@ import { throttle } from "lodash";
 
 const iframe = document.querySelector('iframe');
 const LOCALSTORAGE_KEY = "videoplayer-current-time";
-
 // console.log("🚀 ~ iframe", iframe);
-
 const player = new Player(iframe);
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay(event) {
     // console.log("🚀 ~ e", event.seconds);   
-localStorage.setItem(LOCALSTORAGE_KEY, event.seconds);
+    localStorage.setItem(LOCALSTORAGE_KEY, event.seconds);
 };
-player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));   
+const getLocalStorage = localStorage.getItem(LOCALSTORAGE_KEY); 
+
+if (getLocalStorage === null || getLocalStorage === undefined) {
+    return ;
+} else {
+    player.setCurrentTime(getLocalStorage);
+};
